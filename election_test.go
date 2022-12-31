f97ed3e93a0ddd8a2f11888df53f024f7e587bf2
dbadoy4874 <dbadoy4874@gmail.com>
2022-12-31 11:14:50 +0900

    test: apply new method name `New`

--- election_test.go
+++ election_test.go
@@ -7,11 +7,11 @@ import (
 )
 
 func TestSeungbae(t *testing.T) {
-	e1, _ := NewElection(&Config{"127.0.0.1:8551", []string{"127.0.0.1:8552"}, nil})
-	e2, _ := NewElection(&Config{"127.0.0.1:8552", []string{"127.0.0.1:8551"}, nil})
-	e3, _ := NewElection(&Config{"127.0.0.1:8553", []string{"127.0.0.1:8551", "127.0.0.1:8552"}, nil})
-	e4, _ := NewElection(&Config{"127.0.0.1:8554", []string{"127.0.0.1:8551", "127.0.0.1:8552", "127.0.0.1:8552"}, nil})
-	e5, _ := NewElection(&Config{"127.0.0.1:8555", []string{"127.0.0.1:8551", "127.0.0.1:8552", "127.0.0.1:8553", "127.0.0.1:8554"}, nil})
+	e1, _ := New(&Config{"127.0.0.1:8551", []string{"127.0.0.1:8552"}, nil})
+	e2, _ := New(&Config{"127.0.0.1:8552", []string{"127.0.0.1:8551"}, nil})
+	e3, _ := New(&Config{"127.0.0.1:8553", []string{"127.0.0.1:8551", "127.0.0.1:8552"}, nil})
+	e4, _ := New(&Config{"127.0.0.1:8554", []string{"127.0.0.1:8551", "127.0.0.1:8552", "127.0.0.1:8553"}, nil})
+	e5, _ := New(&Config{"127.0.0.1:8555", []string{"127.0.0.1:8551", "127.0.0.1:8552", "127.0.0.1:8553", "127.0.0.1:8554"}, nil})
 	e1.Run()
 	e2.Run()
 
@@ -44,18 +44,28 @@ func TestSeungbae(t *testing.T) {
 	}()
 
 	go func() {
-		time.Sleep(25 * time.Second)
+		time.Sleep(20 * time.Second)
 		fmt.Println("** register node 5 **")
 		e1.memberlist["127.0.0.1:8555"] = struct{}{}
 		e2.memberlist["127.0.0.1:8555"] = struct{}{}
 		e3.memberlist["127.0.0.1:8555"] = struct{}{}
 		e4.memberlist["127.0.0.1:8555"] = struct{}{}
+
 		e5.Run()
+	}()
+
+	go func() {
+		time.Sleep(25 * time.Second)
+		fmt.Println("Split brain - node 4")
+		delete(e4.memberlist, "127.0.0.1:8551")
+		delete(e4.memberlist, "127.0.0.1:8552")
+		delete(e4.memberlist, "127.0.0.1:8553")
+		delete(e4.memberlist, "127.0.0.1:8555")
 	}()
 
 	done := make(chan struct{})
 	go func() {
-		time.Sleep(30 * time.Second)
+		time.Sleep(35 * time.Second)
 		close(done)
 	}()
 
@@ -70,7 +80,7 @@ func TestSeungbae(t *testing.T) {
 			fmt.Println("done")
 			return
 		default:
-			time.Sleep(time.Second)
+			time.Sleep(500 * time.Millisecond)
 			fmt.Println("e1", e1.state.role().String(), e1.state.term())
 			fmt.Println("e2", e2.state.role().String(), e2.state.term())
 			fmt.Println("e3", e3.state.role().String(), e3.state.term())
